0b2cc7aad50f5f0731fadfda08dd47d1e1aeed90
MarieBsr <basler_marie@yahoo.com>
2025-03-13 20:29:59 +0100

    optimizing code

--- include/developer.h
+++ include/developer.h
@@ -4,16 +4,20 @@
 #include <string.h>
 #include <stdio.h>
 
+//define maximum length for names and alias
 #define MAX_NAME_LENGTH 128
 #define MAX_ALIAS_LENGTH 128
 
+//define type for name and alias
 typedef struct {
     char name[MAX_NAME_LENGTH];
     char alias[MAX_NAME_LENGTH];
 } developer;
 
+//introduce function to list developers
 developer developer_create(char *name, char *alias);
 
+//introduce function to print developers
 void developer_print(developer dev); 
 
 #endif
--- src/developer.c
+++ src/developer.c
@@ -1,5 +1,6 @@
 #include "developer.h"
 
+//copy names and alias in structure
 developer developer_create(char *name, char *alias) {
     developer dev;
     strcpy(dev.name, name);
@@ -7,6 +8,7 @@ developer developer_create(char *name, char *alias) {
     return dev;
 }
 
+//print names and alias 
 void developer_print(developer dev) {
     printf("%s\n", dev.name);
     printf("%s\n", dev.alias);
